c5c1735ddeacff0cd33d490fedb0451ab011f391
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-10-16 10:03:03 +0200

    Swiper Update

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-let slidePosition = 1;
-SlideShow(slidePosition);
+const swiper = new Swiper(".swiper", {
+  // loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  // spaceBetween: 100,
+  // autoplay: { delay: 2000 },
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow((slidePosition += n));
-}
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-//  images controls
-function currentSlide(n) {
-  SlideShow((slidePosition = n));
-}
-
-function SlideShow(n) {
-  let i;
-  let slides = document.getElementsByClassName("containers");
-  let circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {
-    slidePosition = 1;
-  }
-  if (n < 1) {
-    slidePosition = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace(" enable", "");
-  }
-  slides[slidePosition - 1].style.display = "block";
-  circles[slidePosition - 1].className += " enable";
-}
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
