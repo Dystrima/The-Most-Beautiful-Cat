aa6ae93d0c0156d68806a21f85ff3bdbcf5189f6
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-11-15 13:41:42 +0100

    Contact page Update

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const swiper = new Swiper(".swiper", {
-  // loop: true,
-  slidesPerView: 1,
-  centeredSlides: true,
-  // spaceBetween: 100,
-  // autoplay: { delay: 2000 },
+const swiper = new Swiper(".swiper-intro", {
+	// loop: true,
+	slidesPerView: 1,
+	centeredSlides: true,
+	// spaceBetween: 100,
+	// autoplay: { delay: 2000 },
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+
+const swiper2 = new Swiper(".swiper-messages", {
+	loop: true,
+	slidesPerView: 2,
+	centeredSlides: true,
+	// spaceBetween: 100,
+	autoplay: { delay: 2000 },
+
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
 });
