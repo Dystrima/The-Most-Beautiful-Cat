f974dbd8beb1a89edccae042078d2fc66ec8bdd7
Dystrima <138579225+Dystrima@users.noreply.github.com>
2024-02-05 19:29:06 +0100

    Contact page is done !!

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
 	.connect(dbURI)
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,24 +1,28 @@
 const Message = require("../models/Message");
 
-module.exports = {
-	getIndex: (req, res) => {
-		res.render("index");
-	},
-	getPortfolio: (req, res) => {
-		res.render("portfolio");
-	},
-	getAbout: (req, res) => {
-		res.render("about");
-	},
-	getContact: async (req, res) => {
-		// const messages = await Message.find();
-		// res.render("contact", { messages });
-		res.render("contact");
-	},
-	postContact: async (req, res) => {
-		const { name, subject, message } = req.body;
-		console.log(name, subject, message);
-		new Message({ name, subject, message }).save();
-		res.render("contact");
-	},
+module.exports.getIndex = (req, res) => {
+	res.render("index");
+};
+
+module.exports.getPortfolio = (req, res) => {
+	res.render("portfolio");
+};
+
+module.exports.getAbout = (req, res) => {
+	res.render("about");
+};
+
+module.exports.getContact = async (req, res) => {
+	const messages = await Message.find();
+	res.render("contact", { messages });
+};
+
+module.exports.postContact = async (req, res) => {
+	const { name, subject, message } = req.body;
+	try {
+		const newMessage = await Message.create({ name, subject, message });
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
+	res.render("contact");
 };
